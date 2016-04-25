a672d922454e6cb38c9a7d2da4319a534933bcb7
Alain Meurant <alain.meurant@affinitic.be>
2016-04-26 01:14:03 +0200

    ipplf :: formulaire de demande d'intervention
    - JS qui permet de gérer le formulaire selon que le  demandeur et proprio du bien ou pas

--- ipplf/core/browser/js/ipplf.js
+++ ipplf/core/browser/js/ipplf.js
@@ -2,3 +2,20 @@
 script js for IPPLF projet
 AFFINITIC MARS 2016
 */
+
+
+function changementProprio(etat) {
+    var demandeurProprio = document.querySelector('input[name = "demandeurProprio"]:checked').value;
+
+    /* categorie individuelle => reduction_montant et reduction_modif */
+    if (demandeurProprio == "True"){
+        document.getElementById("proprio-demandeur").style="display:block";
+        document.getElementById("motif-demande").style="display:block";
+        document.getElementById("proprio-pas-demandeur").style="display:none";
+    }
+    else {
+        document.getElementById("proprio-demandeur").style="display:none";
+        document.getElementById("motif-demande").style="display:block";
+        document.getElementById("proprio-pas-demandeur").style="display:block";
+    }
+}
